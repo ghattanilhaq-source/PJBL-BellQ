6c6b805d312fe47d0e70b16fbefefecafc6cd8a9
biyyuu <r44zk4s@gmail.com>
2025-11-12 10:30:36 +0700

    add bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>BelQ website</title>
     <link rel="stylesheet" href="style.css" />
-    <link
-      rel="stylesheet"
-      href="/public/assets/bootstrap-5.3.7-dist/css/bootstrap.min.css"
-    />
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
@@ -167,6 +165,24 @@
 
 .activity-card .text-bottom {
   margin-top: 20px;
+}
+
+.badge {
+  margin-bottom: 10px;
+}
+
+.info {
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+}
+
+.activity-card {
+  max-width: 80%;
+  height: auto;
 }
     </style>
   </head>
